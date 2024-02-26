b75c5590ad19c63522be6693c5e43208ecf17a69
joshuakto <34743132+joshuakto@users.noreply.github.com>
2024-02-26 15:47:15 +0800

    migrated llama-index to v0.10.12 and added version number in requirements.txt

--- llama_worker.py
+++ llama_worker.py
@@ -1,55 +1,57 @@
-from llama_index import Document
-from llama_index import ServiceContext, get_response_synthesizer
-from llama_index.retrievers import VectorIndexRetriever
-from llama_index.query_engine import RetrieverQueryEngine
-from llama_index.llms import LangChainLLM
-from llama_index.embeddings import LangchainEmbedding
-from llama_index import VectorStoreIndex, GPTListIndex
-from llama_index.composability import ComposableGraph
-from langchain.llms import Cohere, AI21, OpenAIChat
-from langchain.embeddings import CohereEmbeddings, OpenAIEmbeddings
+from llama_index.core import Document, Settings
+from llama_index.core import get_response_synthesizer
+from llama_index.core import VectorStoreIndex
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.llms.langchain import LangChainLLM
+from llama_index.embeddings.langchain import LangchainEmbedding
+from langchain_community.embeddings import CohereEmbeddings, OpenAIEmbeddings
+from langchain_community.llms import cohere, ai21
+from langchain_openai import ChatOpenAI
+
 
 class LlamaWorker:
     # embedding/llm api is a tuple (api_provider, api_key), e.g. ("openai", "your_api_key")
     def __init__(self, embedding_api, llm_api):
         self.embedding_api_provider, self.embedding_api_key = embedding_api
         self.llm_api_provider, self.llm_api_key = llm_api
-        self.llm_embed = self._create_langchain_embedding(self.embedding_api_provider, self.embedding_api_key)
-        self.llm_command = self._create_langchain_llm(self.llm_api_provider, self.llm_api_key)
-        self.service_context = ServiceContext.from_defaults(embed_model=self.llm_embed,llm=self.llm_command)
-
-
+        self.llm_embed = self._create_langchain_embedding(
+            self.embedding_api_provider, self.embedding_api_key)
+        self.llm_command = self._create_langchain_llm()
+        Settings.llm = self.llm_command
+        Settings.embed_model = self.llm_embed
+        self.service_context = Settings
 
     # currently only support OpenAI and Cohere
+
     @staticmethod
     def _create_langchain_embedding(api_provider, api_key):
         if api_provider == "Cohere":
             return LangchainEmbedding(
                 langchain_embeddings=CohereEmbeddings(
-                model='embed-multilingual-v2.0', cohere_api_key=api_key))
+                    model='embed-multilingual-v2.0', cohere_api_key=api_key))
         if api_provider == "OpenAI":
             return LangchainEmbedding(
                 langchain_embeddings=OpenAIEmbeddings(
-                model='text-embedding-ada-002', openai_api_key=api_key))
+                    model='text-embedding-ada-002', openai_api_key=api_key))
         raise ValueError(f"Unsupported embedding api provider: {api_provider}")
-        
+
     # currently only support OpenAI and Cohere
-    @staticmethod
-    def _create_langchain_llm(api_provider, api_key):
-        if api_provider == "Cohere":
-            return LangChainLLM(llm=Cohere(model='command', cohere_api_key=api_key))
-        if api_provider == "OpenAI":
-            return LangChainLLM(llm=OpenAIChat(model='gpt-4', openai_api_key=api_key))
-        if api_provider == "AI21":
-            return LangChainLLM(llm=AI21(model='j2-ultra', ai21_api_key=api_key))
-        raise ValueError(f"Unsupported llm api provider: {api_provider}")
-        
+    def _create_langchain_llm(self):
+        if self.llm_api_provider == "Cohere":
+            return LangChainLLM(llm=cohere.Cohere(model='command', cohere_api_key=self.llm_api_key))
+        if self.llm_api_provider == "OpenAI":
+            return LangChainLLM(llm=ChatOpenAI(model='gpt-3.5-turbo-0125', openai_api_key=self.llm_api_key))
+        if self.llm_api_provider == "AI21":
+            return LangChainLLM(llm=ai21.AI21(model='j2-ultra', ai21_api_key=self.llm_api_key))
+        raise ValueError(
+            f"Unsupported llm api provider: {self.llm_api_provider}")
+
     def create_query_engine(self, context_list):
         retriever = self.generate_retriever_from_publications(context_list)
         # configure response synthesizer
         response_synthesizer = get_response_synthesizer(
             response_mode="tree_summarize",
-            service_context=self.service_context
         )
         # assemble query engine
         query_engine = RetrieverQueryEngine(
@@ -60,64 +62,11 @@ class LlamaWorker:
 
     def generate_retriever_from_publications(self, context_list):
         pub_index = self.generate_vector_index_from_publications(context_list)
-        pub_retriever = VectorIndexRetriever(pub_index, service_context=self.service_context)
+        pub_retriever = VectorIndexRetriever(
+            pub_index, service_context=self.service_context)
         return pub_retriever
 
     def generate_vector_index_from_publications(self, context_list):
         documents = [Document(text=context) for context in context_list]
-        pub_index = VectorStoreIndex.from_documents(
-            documents,
-            service_context=self.service_context
-        )
+        pub_index = VectorStoreIndex.from_documents(documents)
         return pub_index
-
-
-        
-"""
-llm_embed  = LangchainEmbedding(langchain_embeddings=CohereEmbeddings(model='embed-multilingual-v2.0'))
-# llm_command  = LangChainLLM(llm=Cohere(model='command'))
-llm_command = LangChainLLM(llm=AI21(model='j2-ultra'))
-service_context = ServiceContext.from_defaults(
-    embed_model=llm_embed,
-    llm=llm_command
-)
-
-def create_query_engine(context_list):
-    retriever = generate_retriever_from_publications(context_list)
-    # configure response synthesizer
-    response_synthesizer = get_response_synthesizer(
-        response_mode="tree_summarize",
-        service_context=service_context
-    )
-    # assemble query engine
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer
-    )
-    return query_engine
-
-def generate_retriever_from_publications(context_list):
-    pub_index = generate_vector_index_from_publications(context_list)
-    pub_retriever = VectorIndexRetriever(pub_index, service_context=service_context)
-    return pub_retriever
-
-def generate_vector_index_from_publications(context_list):
-    documents = [Document(text=context) for context in context_list]
-    pub_index = VectorStoreIndex.from_documents(
-        documents,
-        service_context=service_context
-    )
-    return pub_index
-
-def generate_graph_from_publications():
-    # graph = ComposableGraph.from_indices(
-    #     GPTListIndex,
-    #     [index_set[y] for y in years],
-    #     [summaries[y] for y in years],
-    #     service_context=service_context
-    # )
-    pass
-
-def create_vectorstore_from(db, query, k=4):
-    pass
-"""
--- streamlit_app.py
+++ streamlit_app.py
@@ -35,56 +35,57 @@ def run():
          PDB GPT lets you query a PDB id, and ask an LLM to read its related publication to answer your questions about the protein in the entry, or any molecules in the entry!
     """
     )
-    hcol1, hcol2= st.columns([0.6, 0.4])
+    hcol1, hcol2 = st.columns([0.6, 0.4])
     with hcol1:
-      openai_key = st.text_input("🤖 Input your OpenAI API key for best experience:")
-      st.info(icon="ℹ️", body="Providing an OpenAI API key will improve your experience of the app. By default, this app uses a rate-limited Cohere API or AI21 API call.")
+        openai_key = st.text_input(
+            "🤖 Input your OpenAI API key for best experience:")
+        st.info(icon="ℹ️", body="Providing an OpenAI API key will improve your experience of the app. By default, this app uses a rate-limited Cohere API or AI21 API call.")
 
-      lw = create_llama_worker(openai_key)
-      query_text = st.empty()
+        lw = create_llama_worker(openai_key)
+        query_text = st.empty()
 
-    
-       
-       
     col1, col2 = st.columns(2)
 
     with col1:
-      input_query = st.text_input("#### Enter question to ask about this PDB entry using the selected publication as context.")
-      respose = st.empty()
-      example_query = "What molecules are of interest?"
-      st.markdown(f"###### Example Query: {example_query}")
-      example_response = st.empty()
+        input_query = st.text_input(
+            "#### Enter question to ask about this PDB entry using the selected publication as context.")
+        respose = st.empty()
+        example_query = "What molecules are of interest?"
+        st.markdown(f"###### Example Query: {example_query}")
+        example_response = st.empty()
     context_list, pdb_id = pdb_gpt(col1, col2, query_text)
 
     with hcol2:
-       molbox_size = 270
-       if pdb_id:
-        xyzview = py3Dmol.view(query=f'pdb:{pdb_id}', height=molbox_size, width=molbox_size) 
-        xyzview.setStyle({'cartoon':{'color':'spectrum'}, 'background-color':'yellow'})
-        showmol(xyzview, height=molbox_size, width=molbox_size)
+        molbox_size = 270
+        if pdb_id:
+            xyzview = py3Dmol.view(
+                query=f'pdb:{pdb_id}', height=molbox_size, width=molbox_size)
+            xyzview.setStyle(
+                {'cartoon': {'color': 'spectrum'}, 'background-color': 'yellow'})
+            showmol(xyzview, height=molbox_size, width=molbox_size)
 
-        
     query_engine = lw.create_query_engine(context_list)
     example_response.markdown(f"*{query(query_engine, example_query)}")
     if input_query:
-      respose.markdown(f"*{query(query_engine, input_query)}")
+        respose.markdown(f"*{query(query_engine, input_query)}")
 
 
 @st.cache_resource
 def create_llama_worker(openai_key):
-   if openai_key:
-      return LlamaWorker(embedding_api=("OpenAI", openai_key), llm_api=("OpenAI", openai_key))
-   else:
-      return LlamaWorker(embedding_api=("Cohere", st.secrets["COHERE_API_KEY"]), 
-                         llm_api=("AI21", st.secrets["AI21_API_KEY"]))
+    if openai_key:
+        return LlamaWorker(embedding_api=("OpenAI", openai_key), llm_api=("OpenAI", openai_key))
+    else:
+        return LlamaWorker(embedding_api=("Cohere", st.secrets["COHERE_API_KEY"]), llm_api=("AI21", st.secrets["AI21_API_KEY"]))
+
 
 @st.cache_resource
 def create_query_engine(lw, context_list):
-   return lw.create_query_engine(context_list)
+    return lw.create_query_engine(context_list)
+
 
 @st.cache_data
 def query(_query_engine, input_query):
-      return _query_engine.query(input_query)
+    return _query_engine.query(input_query)
 
 
 if __name__ == "__main__":
